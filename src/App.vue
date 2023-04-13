f88ef8ef953f6a620ce4be55c0af1a1e15b457b5
Arseniy Zhdanov <noob1kxgod@outlook.com>
2023-04-13 23:09:14 +0300

    Remove unneccesary comments. I forgot.

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,3 @@ import NavBar from './components/NavBar.vue';
 <style scoped>
 
 </style>
-
-<!-- i feel like a fucking engineer -->
-<!-- like for real -->
-<!-- i got so much stuff opened my head kinda starting to blow up a bit -->
-<!-- Prayge i got like what 5 days to finish -->
-<!-- including hosting and domain and shit and yes. -->
-<!-- fuck me. -->
